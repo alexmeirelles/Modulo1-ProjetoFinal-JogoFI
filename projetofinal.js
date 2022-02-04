02507b9a804698490001111822d5a06f0d1806f6
alexmeirelles <alemeirelles@msn.com>
2022-02-03 22:53:22 -0300

    atualizacoes

---
+++ projetofinal.js
@@ -0,0 +1,56 @@
+Vitor 62993929889
+Jeff 31993253522
+
+Para hoje
+
+Você deve pensar no tema da sua história, e produzir um esboço de como ela 
+será desenvolvida.
+
+Quem é o personagem? Variaveis dele:
+Guerreiro/Mago
+
+Vida 
+Stamina/Energia
+Experiencia
+Dinheiro? Itens?  
+
+Quais os seus desafios? / Que opções de escolha ele pode ter a cada período?
+
+3 Reinos
+- Floresta Tropical
+* 
+* 
+* Luta chefão -  vida - dinheiro/itens
+
+- Dentro Vulcão/montanha
+* Labirinto - pasagem de tempo
+*
+* Luta chefão minotauro - vida - dinheiro/itens
+
+- Reino Gelo
+* Pescar pela sobrevivencia - 
+*
+* Luta chefão Dragão de gelo - vida - dinheiro/itens
+
+Como essas escolhas vão impactar na história e nas possibilidades de 
+escolha futuras?
+
+
+
+Como você irá controlar o passar do tempo?
+
+Com isso pronto você já pode começar a colocar em código:
+Crie variáveis para armazenar os status do personagem (Fome? Dinheiro? 
+    Saúde? Força?). Que tipo de variável seria a mais adequada para isso?
+Crie perguntas que alterarão esses status para testar sua funcionalidade;
+Crie um laço de repetição que determinará os ciclos em que a história vai 
+se passar. O que acontecerá ao final do ciclo? Como ficarão os status?
+
+
+
+Lembre-se: Use sua criatividade, mas cuidado para não criar um projeto 
+megalomaníaco! Vocês terão um bom tempo para fazer o projeto, mas durante 
+esse tempo com certeza novas ideias surgirão e você precisará fazer várias 
+alterações. A tentação pode ser grande, mas cuidado para não deixar o 
+projeto tomar proporções de modo que não seja possível de ser executado 
+dentro do prazo dado.
