e206aaa591fd4208a2f6fcd2d2f25ca46d194e3d
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-02-07 10:26:01 -0300

    adicionando jogo cara coroa

--- projetofinal.js
+++ projetofinal.js
@@ -47,12 +47,67 @@ console.log(sujeitoPersonagem);
 console.log(`Dano bola de fogo= ${sujeitoPersonagem.bolaDeFogo()}`);
 
 
-
-
-
 //Reino de Gelo
 // O personagem se depara com um velho misterioso que lhe oferece uma rede de pesca para
 // pegar peixes e se alimentar recuperando um pouco de vida/energia
+// Mas para conseguir rede o personagem deve jogar cara ou coroa com o velho
+// O jogo tem 3 rodas
+
+let vitoriaPersonagem = 0;
+let vitoriaVelho = 0;
+
+for (let i = 0; i < 3; i++) {
+  let velho = 0;
+  let personagem = 0;
+  let moeda = 0;
+
+  do {
+    console.log(`\nRodada ${i + 1}\n`);
+    personagem = parseInt(prompt('Escolha uma opção: 1 - Cara | 2 - Coroa: '));
+    if (personagem == 1) {
+      velho = 2;
+    } else if (personagem == 2) {
+      velho = 1;
+    } else {
+      console.log('Opção inválida!! \n');
+    }
+    moeda = Math.floor(2 * Math.random() + 1);
+  } while (personagem != 1 && personagem != 2);
+
+  if (personagem == moeda) {
+    vitoriaPersonagem++;
+  } else {
+    vitoriaVelho++;
+  }
+
+  console.log(
+    `Placar: Velho: ${vitoriaVelho} Personagem: ${vitoriaPersonagem}`
+  );
+}
+
+if (vitoriaPersonagem > vitoriaVelho) {
+  console.log('\nParabens! \nVocê ganhou o duelo e recebeu uma rede de pesca!');
+  //aqui o personagem adicina rede pesca na sacola de itens
+} else {
+  console.log('\nQue pena! \nVocê perdeu o duelo!');
+}
+
+
+// Pescar pela sobrevivencia - Se tiver a rede de pesca pega peixes que restauram de 50% a 70%
+// da vida/energia. Se não tiver a rede de pescar ele usa outro item e pesca so um peixe que restaura até 20%
+
+// if(iten existe na sacola de itens){
+//     faz um random para recuperar de 50% a 70% da vida/energia
+// }else{
+//     faz um randon para recuperar até 20%
+// }
+
+
+// Luta chefão Dragão de gelo - vida - dinheiro/itens
+
+//Implementar aqui a mesma logica dos outros chefões
+
+
 
 
 /*Reino 1 : Floresta Tropical / condado/ 
