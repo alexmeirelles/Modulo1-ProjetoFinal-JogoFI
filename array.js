e83606f9193feac22e4f083dcae730bfa2ebadb8
vitor <vitor@cazebre.com>
2022-02-09 20:41:38 -0300

    ok

---
+++ array.js
@@ -0,0 +1,20 @@
+const sujeitoPersonagem = {
+  nome: 'jorge',
+  vida: 5,
+  stamina: 10,
+  itens: [],
+  porrada: function () {
+    // caso ache melhor um retorno com mais casas decimais, basta aumentar argumento de toFixed
+    const dano =
+      0.3 * this.vida +
+      0.5 * parseFloat((this.stamina * Math.random()).toFixed(1)) +
+      0.2 * this.itens;
+    return dano;
+  },
+};
+
+sujeitoPersonagem.itens.push({
+  nome: "código 0",
+});
+
+console.log(sujeitoPersonagem)
