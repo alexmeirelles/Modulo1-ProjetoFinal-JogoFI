611a099063f32e86ee032640b7166d93d91bb213
alexmeirelles <alemeirelles@msn.com>
2022-02-09 22:18:46 -0300

    arualizacao rodando

--- rascunho.js
+++ rascunho.js
@@ -22,7 +22,7 @@ switch (caminho) {
   case "bosque":
     console.log("Voce optou pelo bosque. Daqui vc tem duas opcoes de itens.");
     var itembosque = prompt(`Quer qual das duas opcoes, que leva a pocao ou o dinheiro?`);
-    if(itembosque.toLowerCase == "pocao"){
+    if(itembosque.toLowerCase() == "pocao"){
       console.log("voce pegou a pocao de item");
       sujeitoPersonagem.itens.push({
         item: "pocao",
@@ -39,7 +39,7 @@ switch (caminho) {
   case "ferreiro":
     console.log("Voce optou pelo Ferreiro. Não é uma caminhada longa, ele faz muitos adornos as elites e não pode se distanciar muito do palácio do Rei. Daqui vc tem duas opcoes..");
     var itemferreiro = prompt(`Quer qual das duas opcoes, Armadura ou Escudo?`);
-    if(itemferreiro.toLowerCase == "armadura"){
+    if(itemferreiro.toLowerCase() == "armadura"){
       console.log("voce pegou a armadura de item");
       sujeitoPersonagem.itens.push({
         item: "armadura",
@@ -56,7 +56,7 @@ switch (caminho) {
   case "exercito":
     console.log("Voce optou ir visitar a artilharia pesada. Você considera que é bom estar armado para sua jornada. Bem pensado, afinal, quais desafios você pode encontrar pelo caminho!? E como transpo-los sem nada na mão!?");
     var itemexercito = prompt(`Você chega e é recepcionado pelo Sargeto Orveu, ele é o responsável pelas armas de combate e já aguardava a sua chegada. Orveu tem uma presença dominante e não é de muitas palavras. Pergunta rapidamente qual será a sua jornada, o que possivelmente você irá enfrentar e pega dos itens a mão. Uma espada e uma foice. Quer qual das duas opcoes, digite foice ou espada: `);
-    if(itemexercito.toLowerCase == "espada"){
+    if(itemexercito.toLowerCase() == "espada"){
       console.log("voce pegou a espada de item");
       sujeitoPersonagem.itens.push({
         item: "espada",
