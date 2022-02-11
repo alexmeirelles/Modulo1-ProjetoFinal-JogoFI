f140257dd1a4761a26196df7cc523ed33276e8f7
alexmeirelles <alemeirelles@msn.com>
2022-02-10 22:26:35 -0300

    atualizacao desafio 3

--- projetofinal.js
+++ projetofinal.js
@@ -207,43 +207,6 @@ console.log(`Você derrotou o Gigante Hércules`);
 
 
 
-
-
-
-
-
-
-let r = false
-let vida = 50
-let dano
-
-while (r == false);
-
-do{
-    dano = +prompt(`Digite o Dano do herói (5 a 10): `);
-    
-    if (dano < 5|| dano > 10 ){;
-        console.log(`Digite um valor entre 5 e 10.`);
-        r = false;
-    }
-    
-    else if (dano >= 5 && dano <= 10){;
-        r = true;
-    }}while (r == false);
-
-let calculo
-
-if(vida%dano == 0){
-    calculo = vida / dano
-}
-else if (vida%dano != 0 ){
-    calculo = (vida/dano) + 1
-}
-
-console.log(`A vida do Troll é de ${vida}Hp`)
-console.log(`O dano do Herói causa -${dano}Hp`)
-
-
 //----------------------------------------------------------------//
 console.clear();
 console.log(`
