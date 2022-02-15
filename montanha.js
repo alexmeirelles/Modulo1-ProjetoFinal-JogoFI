d4dc1a347edbec1ce999318d57ed97a4ff593825
devitorone <vitor@cazebre.com>
2022-02-15 14:45:37 -0300

    ok

--- montanha.js
+++ montanha.js
@@ -8,13 +8,14 @@ var sujeitoPersonagem = {
   maxHP: 15,
   stamina: 10,
   itens: [],
-  skills: ["0-Porrada","1-Bola de Fogo"],
+  skills: ["0-Porrada", "1-Bola de Fogo"],
   skill: [
     function porrada() {
       // caso ache melhor um retorno com mais casas decimais, basta aumentar argumento de toFixed
       const dano =
         0.3 * sujeitoPersonagem.vida +
-        0.5 * parseFloat((sujeitoPersonagem.stamina * Math.random()).toFixed(1)) +
+        0.5 *
+          parseFloat((sujeitoPersonagem.stamina * Math.random()).toFixed(1)) +
         0.2 * sujeitoPersonagem.itens;
       return dano;
     },
@@ -22,7 +23,8 @@ var sujeitoPersonagem = {
       // caso ache melhor um retorno com mais casas decimais, basta aumentar argumento de toFixed
       const dano =
         0.7 * sujeitoPersonagem.vida +
-        0.5 * parseFloat((sujeitoPersonagem.stamina * Math.random()).toFixed(1)) +
+        0.5 *
+          parseFloat((sujeitoPersonagem.stamina * Math.random()).toFixed(1)) +
         0.2 * sujeitoPersonagem.itens;
       return dano;
     },
@@ -33,11 +35,6 @@ var sujeitoPersonagem = {
   },
 };
 
-// var cerberino = {
-//   defesa: 5,
-//   ataque: 10,
-//   vida: 60,
-// };
 
 console.log(`
 ${sujeitoPersonagem.status()}
@@ -45,39 +42,41 @@ ${sujeitoPersonagem.status()}
 "Abandone toda a esperança, você que entra por esses portões."
 
 Você encontra uma figura raquítica, um senhor, que diz: Antes de mais nada você tem que responder 
-algumas perguntas (nunca se sabe, de repente você ganha algo)! Será que conhece o inferno?!?!?
+algumas perguntas (nunca se sabe, de repente você ganha algo)! 
+Será que conhece o inferno?!?!?
 Você se parece muito com Dante. Boa sorte!`);
 
 avancar();
-console.log(`${sujeitoPersonagem.status()}
-`);
+console.log(`${sujeitoPersonagem.status()}`);
 
 var p1r2 = prompt("Quantos níveis tem nessa montanha? ");
 var p2r2 = prompt("Qual o crime mais grave desta região? ");
 var p3r2 = prompt("Quem sou eu? ");
-
-if(p1r2)
-//se responder correto dar a skill boladefogo
-
 console.log(`Você acaba de conhecer Caronte, o barqueiro. Sujeito grave, taciturno, "poucas ideias".
 
 Neste reino tudo o que te aguarda são desafios e desventuras. Prepare-se.`);
 
 avancar();
+
 do {
   console.log(`${sujeitoPersonagem.status()}
 
     Perigo!!!!
-  Você dá de cara com um cachorro gigantesco, de 3 cabeças. Sem dúvidas, era Cerberino, o cão abestado.`);
+  Você dá de cara com um cachorro gigantesco, de 3 cabeças. Sem dúvidas,
+  era Cerberino, o cão abestado.`);
+  avancar();
+  fight("Cerberino", 100, 10, 9);
 
-  fight('Cerberino', 100, 10, 9);
+  console.log(`Após derrotar Cerberino, você entra no nível em que estão os apostadores. Hora de testar
+  sua sorte.`);
 
-  console.log()
+  diceGame(3);
 
   var again = prompt("Você gostaria de seguir para o proximo reino? ");
 } while (again == "nao" || again == "n");
 
-console.log(sujeitoPersonagem);
+
+// ************************** FUNÇÕES **************************************************
 
 function avancar() {
   console.log();
@@ -92,55 +91,142 @@ function fight(mobNAME, mobHP, mobDEF, mobATQ) {
   let mobLife = mobHP;
   do {
     console.log();
-    console.log(`Sua vida: `,sujeitoPersonagem.vida);
+    console.log(`Sua vida: `, sujeitoPersonagem.vida);
     console.log();
-    console.log(`HP de ${mobNAME}: `,mobLife.toFixed(1));
+    console.log(`HP de ${mobNAME}: `, mobLife.toFixed(1));
     console.log();
     console.log(sujeitoPersonagem.skills);
-    do{
+    do {
       var skillChoice = +prompt(`Utilizar qual skill? `);
-    }while(Number.isInteger(skillChoice)==false || skillChoice > (sujeitoPersonagem.skill.length)-1);
-    mobLife -= (sujeitoPersonagem.skill[skillChoice]()-mobDEF);
+    } while (
+      Number.isInteger(skillChoice) == false ||
+      skillChoice > sujeitoPersonagem.skill.length - 1
+    );
+    mobLife -= sujeitoPersonagem.skill[skillChoice]() - mobDEF;
     sujeitoPersonagem.vida -= mobATQ;
   } while (mobLife > 0 && sujeitoPersonagem.vida > 0);
-  if(mobLife > 0){
-    console.log('Você quase morreu/morreu.')
-
-  }else if(sujeitoPersonagem.vida>0){
+  if (mobLife > 0) {
+    console.log("Você quase morreu/morreu.");
+  } else if (sujeitoPersonagem.vida > 0) {
     console.log(`Você derrotou ${mobNAME}. Parabéns`);
   }
 }
 
-function diceGame(rounds,){
-  do{
-    var diceChoice= +prompt('Escolha o tamanho do dado a ser usado(somente número) D =>'); 
-  }while(Number.isInteger(diceChoice) ==false);
-    for (let j = 0; j < rounds; j++) {
-    let rodada = [];
-    for (let i = 0; i < players; i++) {
-      do {
-        var p1 = prompt(`Jogar dado, jogador ${i + 1} ?`);
-      } while (
-        p1 == "n" ||
-        p1 == "nao" ||
-        p1 == "not" ||
-        p1 == "0" ||
-        p1 == "no" 
-      );
-      let dado = Math.ceil(Math.random() * diceChoice);
-      console.log("dado", dado);
-      players[i].jogada.push(dado);
-      rodada.push(dado);
+function diceGame(rounds) {
+  do {
+    let zoa = 0;
+    var diceChoice = +prompt(
+      "Escolha o tamanho do dado a ser utilizado (somente número) D =>"
+    );
+    if (Number.isInteger(diceChoice) == false) {
+      console.log("Somente números.");
+      if (zoa > 1) {
+        console.log(
+          "Virgilio: Você está de brinquedo com a minha cara?!  ESCOLHE UM NÚMERO kkk"
+        );
+      }
+      zoa++;
     }
-    rodada.sort((a, b) => {
+  } while (Number.isInteger(diceChoice) == false);
+  var playerN = +prompt("Contra quantos jogadores você quer jogar? ");
+  var players = [
+    {
+      nome: p1,
+      jogada: [],
+      vitorias: 0,
+    },
+    {
+      nome: "Negreanu",
+      jogada: [],
+      vitorias: 0,
+    },
+    {
+      nome: "Phill Ivey",
+      jogada: [],
+      vitorias: 0,
+    },
+    {
+      nome: "Jack Black",
+      jogada: [],
+      vitorias: 0,
+    },
+    {
+      nome: "Amarillo Slim",
+      jogada: [],
+      vitorias: 0,
+    },
+    {
+      nome: "RNGesus, Himself",
+      jogada: [],
+      vitorias: 0,
+    },
+  ];
+  for (let j = 0; j < rounds; j++) {
+    var turn = [];
+
+    playerList = [];
+    for (let m = 1; m < playerN + 1; m++) {
+      playerList.push(players[m].nome);
+    }
+    console.log(`Você está jogando contra ${playerList}`);
+
+    do {
+      var p11 = prompt(`Jogar dado, ${p1} ?`);
+    } while (
+      p11 == "n" ||
+      p11 == "nao" ||
+      p11 == "not" ||
+      p11 == "0" ||
+      p11 == "no"
+    );
+    let dado = Math.ceil(Math.random() * diceChoice);
+    console.log("dado", dado);
+    players[0].jogada.push(dado);
+    turn.push(dado);
+    //outros jogadores
+    for (let i = 1; i < playerN + 1; i++) {
+      let dado = Math.ceil(Math.random() * diceChoice);
+      players[i].jogada.push(dado);
+      turn.push(dado);
+    }
+    console.log(players);
+    console.log(turn);
+    turn.sort((a, b) => {
       return b - a;
     });
-    if (rodada[0] != rodada[1]) {
-      for (let k = 0; k < players.length; k++) {
-        if (players[k].jogada[j] == rodada[0]) {
-          players[k].vitorias++;
+    console.log(turn, "apos sort");
+    if (turn[0] != turn[1]) {
+      for (let l = 0; l < playerN + 1; l++) {
+        if (players[l].jogada[j] == turn[0]) {
+          players[l].vitorias++;
         }
       }
+    }
+  }
+  players.sort((a, b) => {
+    return b.vitorias - a.vitorias;
+  });
+
+  if (players[0].vitorias != players[1].vitorias) {
+    console.log("Ganhador =>", players[0].nome);
+  } else {
+    console.log("Nao houve ganhador.");
+  }
+
+  if (players[0].nome == p1) {
+    console.log("Você ganhou, cabra da peste. ");
+    //adicionar recompensas nos cases abaixo
+    switch (playerN){
+      case 1: 
+        console.log("recompensa 1");
+      case 2: 
+        console.log("recompensa 2");
+      case 3: 
+        console.log("recompensa 3");
+      case 4: 
+        console.log("recompensa 4");
+      case 5: 
+        console.log("recompensa 5");
     }
   }
 }
